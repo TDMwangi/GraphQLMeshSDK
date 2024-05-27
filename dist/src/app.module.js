b2b4ae1c2fed0ca09e3620ed28c11ef34a07281d
TDMwangi <teddymwangi65@gmail.com>
2024-05-27 03:52:22 +0300

    feat: add app module

---
+++ dist/src/app.module.js
@@ -0,0 +1,26 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const countries_module_1 = require("./modules/countries.module");
+const anime_module_1 = require("./modules/anime.module");
+const pets_module_1 = require("./modules/pets.module");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            countries_module_1.CountriesModule,
+            anime_module_1.AnimeModule,
+            pets_module_1.PetsModule,
+        ],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
