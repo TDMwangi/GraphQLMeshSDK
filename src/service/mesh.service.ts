ff7d3037138156751843327bd2aea01c7c673bd2
TDMwangi <teddymwangi65@gmail.com>
2024-06-11 04:16:58 +0300

    feat: add apiservice class

--- src/service/mesh.service.ts
+++ src/service/mesh.service.ts
@@ -1,2 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { getMeshSDK, Sdk } from '../../.mesh';
+
+@Injectable()
+export class ApiService implements OnModuleInit {
+  private sdk: Sdk;
+
+  async onModuleInit() {
+    this.sdk = getMeshSDK();
+  }
+}
